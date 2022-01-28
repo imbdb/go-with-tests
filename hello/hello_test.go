bb36d50b639e337581b0b5c66f44dff43beed8a0
Bharat D Bhadresha <bharat@middleware.io>
2022-01-28 19:55:10 +0530

    handle empty string in greeting funciton hello

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,25 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("imbdb")
-	want := "Hello, imbdb!"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	// Reducing duplication
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	// Sub tests
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("imbdb")
+		want := "Hello, imbdb!"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("saying 'Hello, World!' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World!"
+		assertCorrectMessage(t, got, want)
+	})
+
 }

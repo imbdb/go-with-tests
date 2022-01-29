06ed54d057e4b0b67e0d492b6c5c646fb78c4503
Bharat D Bhadresha <bharat@middleware.io>
2022-01-29 10:02:12 +0530

    sum of numbers using slices

--- arraynslices/sum_test.go
+++ arraynslices/sum_test.go
@@ -3,12 +3,16 @@ package arraynslices
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	expected := 15
+	t.Run("sum of collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
 
-	if got != expected {
-		t.Errorf("expected '%d' but got '%d', given %v", expected, got, numbers)
-	}
+		got := Sum(numbers)
+		expected := 6
+
+		if got != expected {
+			t.Errorf("expected '%d' but got '%d', given %v", expected, got, numbers)
+		}
+	})
+
 }

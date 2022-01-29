0f6c08aa4278998d580b3bc96faf210cb0ec0a8e
Bharat D Bhadresha <bharat@middleware.io>
2022-01-29 12:02:50 +0530

    add tailsum and refactor tests

--- arraynslices/sum_test.go
+++ arraynslices/sum_test.go
@@ -20,11 +20,41 @@ func TestSum(t *testing.T) {
 
 }
 
-func TestSumAll(t *testing.T) {
-	got := SumAll([]int{1, 2}, []int{0, 9})
-	want := []int{3, 9}
-
+func checkSums(t testing.TB, got, want []int) {
+	t.Helper()
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
 	}
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("Sum of some slices", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAll([]int{}, []int{0, 9})
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
+	})
+}
+
+func TestSumAllTail(t *testing.T) {
+	t.Run("Tail Sum of some slices", func(t *testing.T) {
+		got := SumAllTail([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely tail sum empty slices", func(t *testing.T) {
+		got := SumAllTail([]int{}, []int{0, 9})
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
+	})
 }

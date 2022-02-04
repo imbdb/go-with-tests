1e3027afa1f15341804bc1fe302d45eb033ddd69
Bharat D Bhadresha <bharat@middleware.io>
2022-02-04 22:56:17 +0530

    fast url with timeout

--- select/racer.go
+++ select/racer.go
@@ -1,15 +1,26 @@
 package selectpkg
 
 import (
+	"errors"
 	"net/http"
+	"time"
 )
 
-func Racer(url1, url2 string) string {
+var ErrTimeout = errors.New("Request timed out")
+var timeout = 10 * time.Second
+
+func Racer(url1, url2 string) (string, error) {
+	return ConfigurableRacer(url1, url2, timeout)
+}
+
+func ConfigurableRacer(url1, url2 string, timeout time.Duration) (string, error) {
 	select {
 	case <-ping(url1):
-		return url1
+		return url1, nil
 	case <-ping(url2):
-		return url2
+		return url2, nil
+	case <-time.After(timeout):
+		return "", ErrTimeout
 	}
 }
 
--- select/racer_test.go
+++ select/racer_test.go
@@ -11,25 +11,35 @@ import (
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(delay * time.Millisecond)
+		time.Sleep(delay)
 		w.WriteHeader(http.StatusOK)
 	}))
 }
 
 func TestRacer(t *testing.T) {
-	slowServer := makeDelayedServer(20)
-	fastServer := makeDelayedServer(0)
-
-	defer slowServer.Close()
-	defer fastServer.Close()
 
 	Convey("Should return faster url", t, func() {
-		slowURL := slowServer.URL
-		fastURL := fastServer.URL
+		slowServer := makeDelayedServer(2 * time.Millisecond)
+		fastServer := makeDelayedServer(0)
 
-		want := fastURL
-		got := Racer(slowURL, fastURL)
+		defer slowServer.Close()
+		defer fastServer.Close()
 
+		want := fastServer.URL
+		got, err := Racer(slowServer.URL, fastServer.URL)
+		So(err, ShouldBeNil)
 		So(got, ShouldEqual, want)
+	})
+
+	Convey("returns an error if a server doesn't respond within 10s", t, func() {
+		serverA := makeDelayedServer(2 * time.Millisecond)
+		serverB := makeDelayedServer(3 * time.Millisecond)
+
+		defer serverA.Close()
+		defer serverB.Close()
+
+		_, err := ConfigurableRacer(serverA.URL, serverB.URL, 1*time.Millisecond)
+
+		So(err, ShouldEqual, ErrTimeout)
 	})
 }
